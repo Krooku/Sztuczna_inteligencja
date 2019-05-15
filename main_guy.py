e4cef4c9f3895142fa6b1de06de0770548abe0ed
Krooku <kr_oo_ku@o2.pl>
2019-05-15 12:05:54 +0200

    enitity monster slime main character

---
+++ main_guy.py
@@ -0,0 +1,29 @@
+import pygame
+import entity
+
+TILESIZE = 32
+
+class Main_guy(entity.Entity):
+    def __init__(self, x, y):
+        entity.__init__(self)
+        self.s = Main_guy_Sprite(self, self.x, self.y)
+
+    def use(self, benefitor):
+        # TODO
+        self.die()
+
+    def die(self):
+        self.active = False
+
+class Main_guy_Sprite(pygame.sprite.Sprite):
+    def __init__(self, entity, x, y):
+        self.entity = entity
+        # self.groups = game.all_sprites
+        pygame.sprite.Sprite.__init__(self, pygame.sprite.Group())
+        self.image = pygame.Surface((TILESIZE, TILESIZE))
+        self.image = pygame.image.load("img/main_guy.png")
+        self.image = pygame.transform.scale(self.image, (TILESIZE, TILESIZE))
+        # self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
--- map.py
+++ map.py
@@ -1,5 +1,6 @@
 import pygame
 import potions
+import entity
 
 TILESIZE = 32
 
@@ -85,10 +86,11 @@ class Map:
 
         for line_arr in temp_arr:
             if line_arr[0] == 0:
-                print(line_arr[0])
-                print(line_arr[1])
-                print(line_arr[2])
                 obj = potions.Health_Potion(line_arr[1], line_arr[2])
+                entity_manager.add(obj, line_arr[1], line_arr[2])
+                self.getTileData(obj.x, obj.y).setOccupiedBy(obj)
+            if line_arr[0] == 1:
+                obj = entity.Slime(line_arr[1], line_arr[2])
                 entity_manager.add(obj, line_arr[1], line_arr[2])
                 self.getTileData(obj.x, obj.y).setOccupiedBy(obj)
 
